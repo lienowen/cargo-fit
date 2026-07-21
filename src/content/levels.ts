ad2e582993fa3b144b320ffd31cb37562b5e89df
lienowen <lienowen@outlook.com>
2026-07-21 15:27:19 +0800

    content: register playable level catalog

---
+++ src/content/levels.ts
@@ -0,0 +1,5 @@
+import type { LevelDefinition } from '../domain/game';
+import { level001 } from './level-001';
+import { level002 } from './level-002';
+
+export const levels: readonly LevelDefinition[] = [level001, level002];
