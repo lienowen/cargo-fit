745d513627c80f11877fc172fb601ae4350eb757
lienowen <lienowen@outlook.com>
2026-07-21 15:28:23 +0800

    feat: boot registered level catalog

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import Phaser from 'phaser';
 import './style.css';
 import { GameApplication } from './application/GameApplication';
-import { level001 } from './content/level-001';
+import { levels } from './content/levels';
 import { LocalStorageSaveRepository } from './infrastructure/save';
 import { GameScene } from './presentation/GameScene';
 
-const application = new GameApplication(level001, new LocalStorageSaveRepository());
+const application = new GameApplication(levels, new LocalStorageSaveRepository());
 
 new Phaser.Game({
   type: Phaser.AUTO,
